a75086c3fc0390f710d9a07c14e22c9bd38cafbf
rahadityaputra <rahadit.a.p123@gmail.com>
2025-05-19 09:27:10 +0700

    add abstract method hitungKeliling in BangunDatar.java

--- src/geometri/BangunDatar/BangunDatar.java
+++ src/geometri/BangunDatar/BangunDatar.java
@@ -2,17 +2,11 @@ package geometri.BangunDatar;
 import geometri.Geometri;
 
 public abstract class BangunDatar implements Geometri {
-
     protected double keliling;
-
     protected double luas;
-
     protected String nama;
-
     public  BangunDatar(String nama) {
-
     }
-    @Override
     public abstract void hitungKeliling();
     @Override
     public abstract void hitungLuas();
