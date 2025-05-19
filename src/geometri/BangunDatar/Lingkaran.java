1897ba8193bc7a00193416f711bf99be39762601
rahadityaputra <rahadit.a.p123@gmail.com>
2025-05-19 09:25:42 +0700

    add attr PI

--- src/geometri/BangunDatar/Lingkaran.java
+++ src/geometri/BangunDatar/Lingkaran.java
@@ -3,11 +3,12 @@ package geometri.BangunDatar;
 public class Lingkaran extends BangunDatar {
 
     protected double jariJari;
+    protected final double PI = 3.14;
 
     public Lingkaran(double jariJari) {
         super("Lingkaran");
-
     }
+
     @Override
     public void hitungKeliling() {
     }
